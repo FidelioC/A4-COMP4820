50576b49ce900982569c6fba92d319ec035e458a
FidelioC <fiocheah08@gmail.com>
2024-11-29 17:29:20 -0600

    create graph

--- unit_test.py
+++ unit_test.py
@@ -1,13 +1,34 @@
 import de_bruijn
 
 
-def main():
-    # print(de_bruijn.create_possible_kmers(5, "AGCCGATCAT"))
-    possible_kmers = de_bruijn.create_possible_kmers(4, "ATCGATCGA")
+def test_possible_kmers():
+    print(de_bruijn.create_possible_kmers(5, "AGCCGATCAT"))
+
+
+def test_one_graph_read():
+    possible_kmers = de_bruijn.create_possible_kmers(5, "AGCCGATCAT")
     graph = de_bruijn.create_graph(possible_kmers)
     print(graph)
-
     de_bruijn.visualize_graph(graph)
+
+
+def test_all_graph_read():
+    all_reads = [
+        "AGCCGATCAT",
+        "CCGATCATC",
+        "GCCGAT",
+        "AGCCGA",
+        "GATCATGGTCA",
+        "GATCAAGGTCA",
+    ]
+    graph = de_bruijn.create_graph_all_reads(all_reads, 5)
+    de_bruijn.visualize_graph(graph)
+
+
+def main():
+    # test_possible_kmers()
+    # test_one_graph_read()
+    test_all_graph_read()
 
 
 if __name__ == "__main__":
